8330855d0db686e6744a1c3793b593ba449bf3d0
Kincaidr <kincaidr@ymail.com>
2025-06-18 19:17:03 +0200

    cross amtch new

--- cross_match.py
+++ cross_match.py
@@ -1,39 +1,29 @@
+
 import numpy as np
-import matplotlib.pyplot as pl
-from astropy.io import fits
+from astropy.coordinates import SkyCoord, match_coordinates_sky
+from astropy import units as u
 from astropy.table import Table
-from scipy.optimize import curve_fit
 
-def func2(x,m,c): 
-    lsq=(m*x) + c 
-    return lsq 
+def cross_match(tab1,tab2, radius):
+    table1=Table.read(tab1)
+    table2=Table.read(tab2)
+    ra1,ra2 = table1['RA'], table2['RA']
+    dec1,dec2 = table1['DEC'], table2['DEC']
+    cat1 = SkyCoord(ra=ra1, dec=dec1)
+    cat2 = SkyCoord(ra=ra2, dec=dec2)
+    idx, d2d, d3d = match_coordinates_sky(cat1, cat2)
+    max_sep = radius * u.arcsec
+    mask = d2d < max_sep
+    table1 = table1[~mask]
+    table2 = table2[idx[mask]]
+    table1.write(f'{name}_cut_srl_filter.fits', overwrite=True)
+    table2.write(f'{name}_filtered_table2.fits', overwrite=True)    
+    return table1, table2
 
-hdul=fits.open('A2631_cross_match.fits')
-data=Table(hdul[1].data)
 
-flux1=data['Total_flux_1']
-flux2=data['Total_flux_2']
-
-freq1=941
-freq2=1626
-alpha=[]
-
-for i in range(len(flux1)):
-        S=[flux1[i],flux2[i]]
-        V=[freq1,freq2]
-        V=np.array((V))
-        S=np.array((S)) 
-
-        V=np.log10(V) 
-        S=np.log10(S) 
-        
-        coeff2, var2 = curve_fit(func2,V,S) 
-        index=coeff2[0] 
-        
-        alpha.append(index)
-breakpoint()
-mean=np.mean(alpha)
-print('Mean is', mean)
-# pl.plot(V,S,'ro') 
-# pl.plot(V,yfit,'k-') 
-# print (coeff2)
+if __name__ == "__main__":
+    name='Zwcl2341'  # Change this to the desired cluster name
+    path=f'/home/kincaid/Desktop/Saraswati_codes/{name}/catalogs/'
+    tab1=path+f'{name}_cut_srl.fits'
+    tab2=path+f'{name}_res_srl.fits'
+    cross_match(tab1,tab2, radius=5)
