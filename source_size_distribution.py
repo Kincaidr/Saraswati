a0c28edd3abe00c999e0dc4519f990bf3c438b45
Kincaidr <kincaidr@ymail.com>
2025-07-09 13:20:26 +0200

    source sizr distribution

--- source_size_distribution.py
+++ source_size_distribution.py
@@ -24,18 +24,30 @@ def theta_min(S,sigma, bmaj, bmin,alpha_betas):
 #     theta_min=np.sqrt(bmaj*bmin)*np.sqrt(1/(a+b/(S/4*sigma))-1)
 #     return(theta_min)
 
-def median_size( S,k,m):
-    t=k*S**m
-    return(t)
 
-def binning(flux, nbins):
-    pers = np.linspace(0, 100, nbins+1)
-    #Range_x = np.percentile(flux, pers)
-    Range_x = 10**np.linspace(start=np.log10(flux.min()), stop=np.log10(flux.max()), num=nbins+1)
-    return(Range_x)
+def median_size(S, k, m):
+    S = np.asarray(S)
+    t = np.where(S < 1, 2 * k, 2 * k * S**m)
+    return t
+
+def equal_source_bins(flux,size,nbins):
+    bin_centers = []
+    median_sizes = []
+    percentiles = np.linspace(0, 100, nbins + 1)
+    bin_edges = np.percentile(flux, percentiles)
+
+    for i in range(len(bin_edges) - 1):
+        bin_mask = (flux >= bin_edges[i]) & (flux < bin_edges[i+1])
+        if i == len(bin_edges) - 2:
+            bin_mask = (flux >= bin_edges[i]) & (flux <= bin_edges[i+1])
+
+        if np.any(bin_mask):
+            bin_centers.append(np.median(flux[bin_mask]))
+            median_sizes.append(np.median(size[bin_mask]))
+    return np.array(bin_centers), np.array(median_sizes)
 
 
-def plot(cat, bmaj, bmin, sigma_low, sigma_high):
+def plot(cat, bmaj, bmin, sigma_high,axs,color,alpha_beta_array):
     total_sources=len(cat['Maj'])
     print('Total number of sources', total_sources)
     mask=(cat['Maj']==0)
@@ -43,58 +55,53 @@ def plot(cat, bmaj, bmin, sigma_low, sigma_high):
     min=cat['Min'][~mask]*3600
     maj=cat['Maj'][~mask]*3600
     size=np.sqrt(maj*min)
-    nbins=10
-    Range_x = binning(flux, nbins)
-    bin_centers=[]
-    median_sizes=[]
 
-    for i in range(len(Range_x)-1):
-        bin_mask = (flux >= Range_x[i]) & (flux < Range_x[i+1])
-        if np.any(bin_mask):
-            bin_centers.append((Range_x[i] + Range_x[i+1]) / 2)
-            median_sizes.append(np.median(size[bin_mask]))
+    mask1=flux <1
+    bin_centers1,median_sizes1= equal_source_bins(flux[mask1],size[mask1],nbins=5)
+    bin_centers2,median_sizes2= equal_source_bins(flux[~mask1],size[~mask1],nbins=6)
 
-    bin_centers = np.array(bin_centers)
-    median_sizes = np.array(median_sizes)
-    popt, pcov = curve_fit(median_size, bin_centers, median_sizes, p0=[10, 0.1])
-    k_fit, m_fit = popt
-    print(f"Fitted parameters: k = {k_fit}, m = {m_fit}")
+    bin_centers = np.concatenate((bin_centers1, bin_centers2))
+    median_sizes = np.concatenate((median_sizes1, median_sizes2))
+    #popt, pcov = curve_fit(median_size, bin_centers, median_sizes, p0=[10, 0.1])
+    #k_fit, m_fit = popt
+    #print(f"Fitted parameters: k = {k_fit}, m = {m_fit}")
     S=np.logspace(-1.5,2,10000)
     flux=cat['Total_flux']*1e3
     min=cat['Min']*3600
     maj=cat['Maj']*3600
     size=np.sqrt(maj*min)
-
-    fig, ax1 = plt.subplots(figsize=(8, 8))
-    plt.scatter(flux, size, s=10, alpha=0.3,color=color)
-    plt.scatter(bin_centers, median_sizes, label='Median size', marker='*',color=color_median)
+    axs.scatter(flux, size, s=10, alpha=0.3,color=color)
+    axs.scatter(bin_centers, median_sizes, label='Median size', marker='*',color=color_median)
     #plt.plot(S,theta_max(S,sigma_low, bmaj, bmin),label="Maximum size", color="green",linestyle="--")
     #plt.plot(S,theta_min(S,sigma_low, bmaj, bmin,alpha_beta_array),label="", color="orange",linestyle="--")
-    plt.plot(S,theta_max(S,sigma_high, bmaj, bmin),label="Maximum size", color="green",linestyle="--")
-    plt.plot(S,theta_min(S,sigma_high, bmaj, bmin,alpha_beta_array),label="Minimum size", color="orange",linestyle="--")
-    plt.plot(S, median_size(S, *popt), label="Windhorst fitted Curve", color="purple")
-    plt.xscale('log')
-    plt.ylim(-2,80)
-    plt.xlabel(r"$S_T$ [mJy]", size=22)
-    plt.ylabel(r"$\Theta$ [arcsec]", size=22)
-    ax1.tick_params(axis='both', which='both', direction='in', length=6 , labelsize=14)  # 'in' means they point inward
-    ax1.tick_params(which='minor', length=3)
+    axs.plot(S,theta_max(S,sigma_high, bmaj, bmin),label="Maximum size", color="green",linestyle="--")
+    axs.plot(S,theta_min(S,sigma_high, bmaj, bmin,alpha_beta_array),label="Minimum size", color="orange",linestyle="--")
+    axs.plot(S, median_size(S, k=4, m=0.03), label=r"$4 \times$ Windhorst model ($m=0.03$)", color="purple")
+    axs.plot(S, median_size(S, k=4, m=0.3), label=r"$4 \times$ Windhorst model ($m=0.3$)", color="grey")
+    axs.set_xscale('log')
+    axs.set_ylim(-2,80)
+    axs.set_xlabel(r"$S_T$ [mJy]", size=22)
+    axs.set_ylabel(r"$\Theta$ [arcsec]", size=22)
+    axs.tick_params(axis='both', which='both', direction='in', length=6 , labelsize=14)  # 'in' means they point inward
+    axs.tick_params(which='minor', length=3)
     #plt.title("Disitrubtion of real soure sizes with fitted median size model")
-    plt.legend(fontsize=16)
-    plt.savefig('/home/kincaid/Desktop/MOSS2_paper/paper/Figures/plots/'+name+'_size_distribution.png', bbox_inches='tight', pad_inches=0.1,dpi=300)
-    plt.show()
+
 
 if "__main__":
-    name='A2631'
-    cat=Table.read('/home/kincaid/Desktop/Saraswati_codes/'+name+'/catalogs/'+name+'_srl_flux_corr.fits')
-    outname='/home/kincaid/Desktop/Saraswati_codes/'+name+'/plots/'
+    names=['A2631','Zwcl2341']
     bmaj=8.7159409207893
     bmin=7.209874015964243
-    sigma_low=0.03
-    sigma_high=0.04
-    alpha_beta_array=[1.1,1.35]  #A2631
+    sigma_high=0.03
+    alpha_beta_array= [[1.03, 1.4], [1.01, 1.7]]
+
     #alpha_beta_array=[1.1,1.7] #Zwcl2341
-    color='blue'
+    colors=['blue','red']
     color_median='cyan'
     threshold=5
-    plot(cat, bmaj, bmin, sigma_low, sigma_high)
+    fig, axs = plt.subplots(1, 2, figsize=(17, 7), sharey=True)
+    for i,name in enumerate(names):
+        cat=Table.read('/home/kincaid/Desktop/Saraswati_codes/'+name+'/catalogs/'+name+'_flux_corr_srl.fits')
+        plot(cat, bmaj, bmin, sigma_high,axs[i],colors[i],alpha_beta_array[i])
+    plt.legend(fontsize=12)
+    plt.savefig('/home/kincaid/Desktop/MOSS2_paper/paper/Figures/plots/size_distribution.png', bbox_inches='tight', pad_inches=0.1,dpi=300)
+    plt.show()
