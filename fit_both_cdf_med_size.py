b9a50e2f988f509b4af3d6bb1861716d17344bc7
Kincaidr <kincaidr@ymail.com>
2025-07-28 17:20:54 +0200

    new

--- fit_both_cdf_med_size.py
+++ fit_both_cdf_med_size.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize, curve_fit
 from scipy.interpolate import interp1d
 import pickle
+from astropy.table import Table
 
 def func(a,b,S,N):
     f=a+b/(S/N)
@@ -90,88 +91,78 @@ def fit_k_m_joint(S_all, sizes_all, bin_centers, observed_medians, bin_size=30):
 def size_dist(S_all, sizes_all,flux_0,sizes_0, bin_centers, observed_medians, k_fit, m_fit, bin_size=30):
     S_grid = np.logspace(np.log10(np.min(S_all)), np.log10(np.max(S_all)), 1000)
     k,m=8,0.3
-    theta_med_1 = median_size2(S_grid, k, m)
+    theta_med_1 = median_size1(S_grid, k, m)
     theta_med_2 = median_size2(S_grid, k_fit, m_fit)
 
-    plt.figure(figsize=(8, 5))
-    plt.scatter(S_all, sizes_all, s=5, alpha=0.3, label='Resolved sources')
-    plt.scatter(flux_0, sizes_0, s=15, alpha=0.3, label='Unresolved sources',color='blue')
+    plt.figure(figsize=(9, 7))
+    plt.scatter(S_all, sizes_all, s=7, alpha=0.3, label='Resolved sources')
+    plt.scatter(flux_0, sizes_0, s=15, alpha=0.3, marker='x',label='Unresolved sources',color='blue')
     plt.scatter(bin_centers, observed_medians, color='red', marker='*', s=100, label='Observed Medians')
-    plt.plot(S_grid, theta_med_1, color='black', linewidth=2, label=r"$\Theta_{\mathrm{med,1}}$")
-    plt.plot(S_grid, theta_med_2, color='purple', linewidth=2, label=r"$\Theta_{\mathrm{med,2}}$",)
-    
-    plt.plot(S_grid,theta_max(S_grid,sigma_low, bmaj, bmin),label="Maximum size", color="green",linestyle="--")
-    plt.plot(S_grid,theta_min(S_grid,sigma_low, bmaj, bmin,alpha_beta_array[0],alpha_beta_array[1]),label="Minimum size", color="orange",linestyle="--")
-    plt.plot(S_grid,theta_max(S_grid,sigma_high, bmaj, bmin), color="green",linestyle="--")
-    plt.plot(S_grid,theta_min(S_grid,sigma_high, bmaj, bmin,alpha_beta_array[0],alpha_beta_array[1]), color="orange",linestyle="--")
+    plt.plot(S_grid, theta_med_1, color='orange', linewidth=2, label=r"$\Theta_{\mathrm{med,1}}$")
+    plt.plot(S_grid, theta_med_2, color='green', linewidth=2, label=r"$\Theta_{\mathrm{med,2}}$",)
+    plt.plot(S_grid,theta_max(S_grid,sigma_low, bmaj, bmin),label="Maximum size", color="purple",linestyle="--")
+    plt.plot(S_grid,theta_min(S_grid,sigma_low, bmaj, bmin,alpha_beta_array[0],alpha_beta_array[1]),label="Minimum size", color="black",linestyle="--")
+    plt.plot(S_grid,theta_max(S_grid,sigma_high, bmaj, bmin), color="purple",linestyle="--")
+    plt.plot(S_grid,theta_min(S_grid,sigma_high, bmaj, bmin,alpha_beta_array[0],alpha_beta_array[1]), color="black",linestyle="--")
     plt.xscale('log')
-    plt.ylim(0,30)
+    plt.ylim(-5,50)
     # plt.yscale('log')
-    plt.xlabel('Flux Density (mJy)')
-    plt.ylabel('Source Size (arcsec)')
-    plt.legend()
+    plt.xlabel(r'$S_T$ (mJy)', size=20)
+    plt.ylabel(r'$\Theta$ (arcsec)', size=20)
+    plt.legend(fontsize=13)
     #plt.grid(True, which='both', ls='--')
     plt.tick_params(axis='both', which='major', direction='in', length=8, width=1, labelsize=16)
     plt.tick_params(axis='both', which='minor', direction='in', length=4, width=1, labelsize=16)
     plt.tight_layout()
+    plt.savefig('/home/kincaid/Desktop/MOSS2_paper/paper/Figures/plots/size_distribution.png', bbox_inches='tight', pad_inches=0.1,dpi=300)
     plt.show()
 
-def CCDF_plot(S_all,sizes_all,bin_size=30):
+def CCDF_plot(sizes_all, bin_size=50, a=-np.log(2), b=0.62):
     theta_vals, ccdf_emp = complementary_ecdf(sizes_all, bin_size=bin_size)
-    theta_med_2 = np.mean(median_size2(S_all, k_fit, m_fit))
-    theta_med_1 = np.mean(median_size1(S_all, k_fit, m_fit))
-    ccdf_model_1 = integral_dist(theta_med_1, theta_vals)
-    ccdf_model_2 = integral_dist(theta_med_2, theta_vals)
-    plt.figure(figsize=(8, 5))
-    plt.plot(theta_vals, ccdf_emp, drawstyle='steps-post', label='Empirical CCDF')
-    plt.plot(theta_vals, ccdf_model_2, '-', label=r"$\Theta_{\mathrm{med,2}}$", linewidth=2)
-    plt.plot(theta_vals, ccdf_model_1, '-', label=r"$\Theta_{\mathrm{med,1}}$", linewidth=2)
-    # plt.xscale('log')
-    # plt.yscale('log')
-    #   plt.xlim(0,30)
-    plt.xlabel('Source Size (arcsec)')
-    plt.ylabel('CCDF')
-    plt.legend()
-    plt.grid(True, which='both', ls='--')
+    min_flux, max_flux = 0.001, 100
+    S_grid = np.logspace(np.log10(min_flux), np.log10(max_flux), 1000)
+    theta_lims_grid = np.logspace(np.log10(1), np.log10(100), 1000)
+    
+    k, m = 8, 0.3
+    theta_med_1 = median_size1(S_grid, k, m)
+    theta_med_2 = median_size2(S_grid, k_fit, m_fit)
+    ccdf_model_1 = integral_dist(theta_med_1, theta_lims_grid, a=a, b=b)
+    ccdf_model_2 = integral_dist(theta_med_2, theta_lims_grid, a=a, b=b)
+
+    theta_lims = theta_lim(S_grid, sigma_low, bmaj, bmin, alpha_beta_array[0], alpha_beta_array[1])
+    ccdf_model_1_lim = integral_dist(theta_med_1, theta_lims, a=a, b=b)
+    ccdf_model_2_lim = integral_dist(theta_med_2, theta_lims, a=a, b=b)
+    c1 = 1 / (1 - ccdf_model_1_lim)
+    c2 = 1 / (1 - ccdf_model_2_lim)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 5))
+    ax1.plot(theta_vals, ccdf_emp, drawstyle='steps-post', linewidth=3)
+    ax1.plot(theta_lims_grid, ccdf_model_1, '-', label=r"$\Theta_{\mathrm{med,1}}$", linewidth=3,color='black')
+    ax1.plot(theta_lims_grid, ccdf_model_2, '-', label=r"$\Theta_{\mathrm{med,2}}$", linewidth=3,color='purple')
+    ax1.axvline(k_fit, ls='--', linewidth=2, color='black')
+    ax1.set_xlabel(r"$\Theta_{\mathrm{lim}} \, \mathrm{[arcsec]}$", size=18)
+    ax1.set_ylabel(r"$h(>\Theta_{\mathrm{lim}})$", size=18)
+    ax1.set_xlim(5, 25)
+    ax1.legend(fontsize=15)
+    ax1.tick_params(axis='both', which='both', direction='in', length=6, width=1)
+
+    ax2.plot(S_grid, c1, label=r"$\Theta_{\mathrm{med,1}}$", linewidth=3,color='orange')
+    ax2.plot(S_grid, c2, label=r"$\Theta_{\mathrm{med,2}}$", linewidth=3,color='green')
+    ax2.set_xlabel(r"$S_T$ [mJy]", size=18)
+    ax2.set_ylabel(r"$c = 1/[1 - h(>\Theta_{\mathrm{lim}})]$", size=18)
+    ax2.set_xscale('log')
+    ax2.legend(fontsize=15)
+    ax2.tick_params(axis='both', which='both', direction='in', length=6, width=1)
+    plt.savefig('/home/kincaid/Desktop/MOSS2_paper/paper/Figures/plots/resolution_bias.png', bbox_inches='tight', pad_inches=0.1,dpi=300)
     plt.tight_layout()
     plt.show()
-    
-def resol_bias():
-    S_vals = np.logspace(-2, 2, 1000) 
-    theta_lims = theta_lim(S_vals, sigma_low, bmaj, bmin, alpha_beta_array[0],alpha_beta_array[1])
-    theta_med_1 = median_size2(S_vals, k_fit, m_fit)
-    theta_med_2 = median_size1(S_vals, k_fit, m_fit)
-    h1=integral_dist(theta_med_1,theta_lims)
-    h2=integral_dist(theta_med_2,theta_lims)
-    c1=1/(1-h1)
-    c2=1/(1-h2)
-    interp_Func1 = interp1d(S_vals, c2, bounds_error=False, fill_value=0)
+
+    interp_Func1 = interp1d(S_grid, c1, bounds_error=False, fill_value='extrapolate')
     with open('/home/kincaid/Desktop/Saraswati_codes/resolution_interp_func.pkl', 'wb') as f:
         pickle.dump(interp_Func1, f)
-    plt.plot(S_vals ,c1,label=r"$\Theta_{\mathrm{med,1}}$",linewidth=3)
-    plt.plot(S_vals ,c2,label=r"$\Theta_{\mathrm{med,2}}$",linewidth=3)
-    plt.ylabel(r"c=$1/[1-h(>\Theta_{\mathrm{lim}})$]", size=24)
-    plt.xlabel(r"$S_T$ [mJy]", size=24)
-    plt.xscale('log')
-    plt.legend(fontsize=22)
-    plt.show()
     return interp_Func1
-
-def correction(interp_func):
-    mask=cat['Maj'] !=0
-    flux=cat['Total_flux'][mask]*1e3
-    corr=interp_func(np.array(flux))
-    S_corr=flux*1/corr
-    cat['Total_flux'][mask] = S_corr*1e-3  # Convert back to Jy
-    outname=f'{path}{name}/catalogs/{name}_resolution_corr_srl.fits'
-    cat.write(outname, overwrite=True)
-    print(f"Resolution bias corrected catalog written to {outname}")
-
-from astropy.table import Table
-
+    
 if __name__=="__main__":
-    name='Zwcl2341'
-    path='/home/kincaid/Desktop/Saraswati_codes/'
     cat=Table.read('/home/kincaid/Desktop/Saraswati_codes/MeerKAT_eddington_combined.fits')
     mask = cat['Maj'] == 0
     flux = cat['Total_flux'][~mask] * 1e3  # mJy
@@ -187,9 +178,10 @@ if __name__=="__main__":
     threshold=5
     mask_low = flux < 1
     sigma_high=0.04
-    sigma_low=0.015
+    sigma_low=0.016
     bmaj=8.7159409207893
     bmin=7.209874015964243
+    alpha_beta_array= [1.1, 1.35]
     alpha_beta_array= [1.1, 1.7] #[1.1, 1.35] #
     bin_centers1, medians1 = equal_source_bins(flux[mask_low], sizes[mask_low], nbins=5)
     bin_centers2, medians2 = equal_source_bins(flux[~mask_low], sizes[~mask_low], nbins=6)
@@ -198,11 +190,10 @@ if __name__=="__main__":
 
     # Fit
     k_fit, m_fit = fit_k_m_joint(flux, sizes,bin_centers, median_sizes, bin_size=50)
-    k_fit, m_fit=8,0.03
+    #k_fit, m_fit=8,0.03
     # Plot
     size_dist(flux, sizes,flux_0,sizes_0,  bin_centers, median_sizes, k_fit, m_fit)
-    CCDF_plot(flux,sizes,bin_size=30)
-    interp_func=resol_bias()
-    correction(interp_func)
+    #interp_func=CCDF_plot(sizes)
+    #correction(interp_func)
     # Output
     print(f"Fitted parameters: k = {k_fit:.4f}, m = {m_fit:.4f}")
