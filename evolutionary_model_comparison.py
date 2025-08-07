378a4a98482daa40db8ba133ffdb6265b16b3f23
Kincaidr <kincaidr@ymail.com>
2025-08-07 13:56:46 +0200

    new

--- evolutionary_model_comparison.py
+++ evolutionary_model_comparison.py
@@ -3,9 +3,13 @@ from scipy.stats import gaussian_kde
 import numpy as np
 import matplotlib.pyplot as plt
 # Generate KDE curve for redshift
-
 from matplotlib.ticker import FuncFormatter
 
+def format_no_decimal(x, pos):
+        if x == int(x):
+            return str(int(x))
+        else:
+            return str(x)
 # Apply to axes
 
 TRECS_area=25
@@ -22,7 +26,7 @@ def redshift_table(flux_min, flux_max):
     HSC2_table = Table.read('/home/kincaid/Desktop/Saraswati_codes/catalogs/Zwcl2341_HSC_dnnz_MeerKAT.fits')
     A2631_redshift_HSC=HSC_table(HSC1_table)
     Zwcl2341_redshift_HSC=HSC_table(HSC2_table)
-    Model_table = Table.read('/home/kincaid/Desktop/Saraswati_codes/catalogs/catalogue_continuum_wrapped.fits')
+    Model_table = Table.read('/home/kincaid/Desktop/Saraswati_codes/catalogs/catalogue_continuum_clustered_wrapped.fits')
     flux_model = Model_table['I1400']
     mask_flux = (flux_model < flux_max) & (flux_model > flux_min)
     mask_z = (Model_table['redshift'] > 0) & (Model_table['redshift'] < 4)
@@ -35,16 +39,19 @@ def redshift_table(flux_min, flux_max):
     return A2631_redshift_HSC,Zwcl2341_redshift_HSC, redshift_all, redshift_SFR, redshift_AGN
 
 
-flux_bins = [(0.1, 0.2), (0.2, 0.4), (0.4, 1), (1, 2), (2, 20), (20, 40)]
+flux_bins = [(0.1, 0.2), (0.2, 0.4), (0.4, 1), (1, 2), (2, 20), (20, 100)]
 fig, axes = plt.subplots(2, 3, figsize=(15, 7), sharey=False)
-bins=[35,35,35,15,15,8  ]
+bins=[50,40,40,10,10,8   ]
+bins_curve=[20,20,20,12,10,7  ]
 for i, (flux_min, flux_max) in enumerate(flux_bins):
     row, col = divmod(i, 3)
     ax = axes[row, col]
-    
     A2631_redshift_HSC,Zwcl2341_redshift_HSC, redshift_all, redshift_SFR, redshift_AGN = redshift_table(flux_min, flux_max)
-    x1, y1 = np.histogram(A2631_redshift_HSC, bins=20, density=False, weights=np.ones_like(A2631_redshift_HSC) / HSC_area)
-    x2, y2 = np.histogram(Zwcl2341_redshift_HSC, bins=20, density=False, weights=np.ones_like(Zwcl2341_redshift_HSC) / HSC_area)
+    print('Total N all',len(redshift_all))
+    print('Total N A2631',len(A2631_redshift_HSC))
+    print('Total N Zwcl2341',len(Zwcl2341_redshift_HSC))
+    x1, y1 = np.histogram(A2631_redshift_HSC, bins=bins_curve[i], density=False, weights=np.ones_like(A2631_redshift_HSC) / HSC_area)
+    x2, y2 = np.histogram(Zwcl2341_redshift_HSC, bins=bins_curve[i], density=False, weights=np.ones_like(Zwcl2341_redshift_HSC) / HSC_area)
     bin_centers1 = 0.5 * (y1[1:] + y1[:-1])
     bin_centers2 = 0.5 * (y2[1:] + y2[:-1])
     ax.plot(bin_centers1, x1, label='A2631', color='blue', linewidth=3)
@@ -55,31 +62,22 @@ for i, (flux_min, flux_max) in enumerate(flux_bins):
             density=False, weights=np.ones_like(redshift_AGN) / TRECS_area, histtype='barstacked', rwidth=0.8)
     
     ax.tick_params(axis='both', which='major', labelsize=12, length=5, width=1)
+    ax.set_title(f'{flux_min}-{flux_max} mJy',fontsize=17)
+    ax.text(0.98, 0.2, f'N = {len(redshift_all)}', transform=ax.transAxes,
+        fontsize=15, ha='right', va='bottom')
+     
+    yticks = ax.get_yticks()
+    ax.set_yticks(yticks.astype(int))
+
     if row ==1:
-        ax.set_xlabel('Redshift',fontsize=16)
-    
-    def format_no_decimal(x, pos):
-        if x == int(x):
-            return str(int(x))
-        else:
-            return str(x)
-
-    formatter = FuncFormatter(format_no_decimal)
-
-    for ax in axes.flatten():
-        ax.xaxis.set_major_formatter(formatter)
-
-
-    ax.set_title(f'{flux_min}-{flux_max} mJy')
+        ax.set_xlabel('Redshift',fontsize=17)
     if i == 0:
-        ax.legend()
-    
+        ax.legend(fontsize=15)
     if col == 0:
-        ax.set_ylabel('N (deg$^{-2}$)',fontsize=16)
+        ax.set_ylabel('N (deg$^{-2}$)', fontsize=17)
     else:
-        ax.set_yticklabels([])
-        ax.set_yticks([])
-
+        ax.set_ylabel('', fontsize=17)
 plt.tight_layout()
+plt.savefig('/home/kincaid/Desktop/MOSS2_paper/paper/Figures/plots/evolutionary_plots.png', bbox_inches='tight', pad_inches=0.1,dpi=300)
 plt.show()
 
